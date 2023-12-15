fdd1f3b0545e764c5bdb14261db26f56d9d394eb
Roflstilzkin <134419803+Roflstilzkin@users.noreply.github.com>
2023-12-14 20:14:37 -0800

    Delete src directory
    use rr director

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $connString = "mysql:host=localhost; dbname=roamready";
-    $user="csusm";
-    $pass="csusm";
-
-    $pdo=new pdo($connString,$user,$pass);
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);//useful during initial development and debugging
-?>
